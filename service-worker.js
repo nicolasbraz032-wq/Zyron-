14c67dffa1ee2d369a62eea06d50e3a45fe6cc39
nicolasbraz032-wq <nicolasbraz032@gmail.com>
2026-08-20 02:38:45 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE = "zyron-3-5-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
